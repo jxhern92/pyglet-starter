400e946aae2826e6e9be5380b81590dd1acb62fa
Javier <60404224+jxhern92@users.noreply.github.com>
2020-02-10 14:18:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ window = pyglet.window.Window()
 
 label = pyglet.text.Label('Hello, world',
                           font_name='Times New Roman',
-                          font_size=36,
+                          font_size=95,
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
 @window.event
